c838aa88b81957c5c140907811adbfe6b6148501
Krishn Sharma <183143060+krishn-exe@users.noreply.github.com>
2026-09-26 18:36:46 +0530

    feat: setup AWS SES

--- KiseKaeDjango/settings.py
+++ KiseKaeDjango/settings.py
@@ -130,10 +130,11 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Email
-# https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
-MAILERS = {
-    'default': {
-        'BACKEND': 'django.core.mail.backends.console.EmailBackend',
-    },
-}
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
